feea9d0f6fa8d225af971e1a0c149d458b39213a
jark <kark@kk.com>
2023-10-03 21:43:20 +0700

    ..

--- egls850wd_test.go
+++ egls850wd_test.go
@@ -10,15 +10,15 @@ func TestHandler100(t *testing.T) {
 
 	f := "c:/w/egls850wd.txt"
 
-	f2 := "egls850wdAbbr.txt"
+	//	f2 := "egls850wdAbbr.txt"
 	lib.ReadLine(f, func(line string) {
 		fmt.Println(line)
 		if lib.Len(line) < 3 {
 			return
 		}
-		firstCh := lib.Left(line, 1)
-		other := lib.Substr(line, 1)
-		lib.Write(f2, line+"\n")
+		//firstCh := lib.Left(line, 1)
+		//other := lib.Substr(line, 1)
+		//lib.Write(f2, line+"\n")
 	})
 	//readFile, _ := os.Open(f)
 	//
