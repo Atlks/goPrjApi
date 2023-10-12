54d0ce948d3a69ec04765056769385021b8d7379
jark <kark@kk.com>
2023-10-12 16:19:10 +0700

    ...

--- lib/sys.go
+++ lib/sys.go
@@ -1,7 +1,47 @@
 package lib
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strconv"
+	"time"
+)
 
 func Echo(s any) {
 	fmt.Println(s)
+}
+
+func IntToStr(xx int) string {
+	return strconv.Itoa(xx)
+}
+
+func Rdm() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(999999999)
+}
+
+func Rdm2() int {
+	n := rand.Int()
+
+	return n
+}
+
+var defaultLetters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+// RandomString returns a random string with a fixed length
+func RandomString(n int, allowedChars ...[]rune) string {
+	var letters []rune
+
+	if len(allowedChars) == 0 {
+		letters = defaultLetters
+	} else {
+		letters = allowedChars[0]
+	}
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return string(b)
 }
